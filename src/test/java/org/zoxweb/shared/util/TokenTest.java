5a247e6a7cd8d38b08f3f4b811e2fbcf1af435bb
javaconsigliere <javaconsigliere@gmail.com>
2017-05-18 10:34:21 -0700

    code cleanup

--- src/test/java/org/zoxweb/shared/util/TokenTest.java
+++ src/test/java/org/zoxweb/shared/util/TokenTest.java
@@ -1,6 +1,10 @@
 package org.zoxweb.shared.util;
 
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
+
+import org.zoxweb.server.security.CryptoUtil;
 
 public class TokenTest
 {
@@ -10,9 +14,22 @@ public class TokenTest
 		uuid.getLeastSignificantBits();
 		
 		
-		byte[] token = BytesValue.LONG.toBytes(null, 0, uuid.getLeastSignificantBits(), uuid.getMostSignificantBits(), uuid.getLeastSignificantBits());
+		byte[] token = BytesValue.LONG.toBytes(null, 0, uuid.getLeastSignificantBits(), uuid.getMostSignificantBits());
 		byte[] b64 = SharedBase64.encode(token);
-		System.out.println(SharedStringUtil.toString(b64) + " " + uuid);
+		byte[] uuidString = SharedBase64.encode(SharedStringUtil.embedText(""+uuid, "-", ""));
+		System.out.println(SharedStringUtil.toString(b64) + " " + uuid + " " +  SharedStringUtil.toString(uuidString));
+		
+		try 
+		{
+			Key key = CryptoUtil.generateKey(384, "HmacSHA256");
+	
+			b64 = SharedBase64.encode(key.getEncoded());
+			System.out.println(SharedStringUtil.toString(b64) );
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 	}
 }
