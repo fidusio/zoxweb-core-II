5002e4c0b5d634c2c78c2c182bac834ec94779a0
zoxweb-dev1 <mzebib@zoxweb.com>
2017-05-20 20:11:40 -0700

    Updated

---
+++ src/main/java/org/zoxweb/shared/data/order/OrderStatus.java
@@ -0,0 +1,15 @@
+package org.zoxweb.shared.data.order;
+
+/**
+ * Created on 5/20/17
+ */
+public enum OrderStatus {
+    CANCEL,
+    COMPLETED,
+    DELIVERED,
+    EXCEPTION,
+    IN_PROGRESS,
+    PENDING,
+    RECEIVED,
+    SENT
+}
