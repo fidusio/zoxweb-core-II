8d26523961c6016bb91ba6a44c545e90be78203e
javaconsigliere <javaconsigliere@gmail.com>
2017-09-07 10:09:05 -0700

    Implemententin JCache for the shiro

---
+++ src/main/java/org/zoxweb/server/security/shiro/cache/ShiroJCache.java
@@ -0,0 +1,54 @@
+package org.zoxweb.server.security.shiro.cache;
+
+import java.util.Collection;
+import java.util.Set;
+
+import org.apache.shiro.cache.Cache;
+import org.apache.shiro.cache.CacheException;
+
+public class ShiroJCache<K, V> implements Cache<K, V>{
+
+	@Override
+	public V get(K key) throws CacheException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V put(K key, V value) throws CacheException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V remove(K key) throws CacheException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clear() throws CacheException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Set<K> keys() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<V> values() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
