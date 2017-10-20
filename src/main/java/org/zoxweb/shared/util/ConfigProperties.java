eb4b7d0f17b0561d1e61bd2a07dd2620b0932607
javaconsigliere <javaconsigliere@gmail.com>
2017-10-20 15:48:29 -0700

    update

--- src/main/java/org/zoxweb/shared/util/ConfigProperties.java
+++ src/main/java/org/zoxweb/shared/util/ConfigProperties.java
@@ -2,6 +2,8 @@ package org.zoxweb.shared.util;
 
 public interface ConfigProperties 
 {
+	
+	
 	ArrayValues<GetNameValue<String>> getProperties();
 	
 	void setProperties(ArrayValues<GetNameValue<String>> aValues);
