3ac4da6a1b986f58a5ea94c2c862360f8849cedf
Tony <tony@mobtekwholesale.com>
2018-01-29 12:35:20 -0800

    Tony's push test 1. Added a comment.

--- src/test/java/org/zoxweb/LambdaTest.java
+++ src/test/java/org/zoxweb/LambdaTest.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2012-2017 ZoxWeb.com LLC.
+ *
+ * Tony Test Comment
+ */
+
+
 package org.zoxweb;
 
 import java.util.Date;
@@ -12,19 +19,19 @@ public class LambdaTest
 			return x-y;
 		}
 	}
-	
+
 	public static void main(String ...args)
 	{
-		
-		
+
+
 		toto t = (int x, int y) -> {return x+y;};
-		
+
 		Runnable r = () -> System.out.println("Tizi " + new Date() + " " + Thread.currentThread() );
-		
+
 		r.run();
 		r.run();
-		
-		
+
+
 		System.out.println("CurrentThread " + Thread.currentThread());
 		System.out.println(t.add(1,2));
 		System.out.println(t.sub(1,2));
@@ -33,6 +40,6 @@ public class LambdaTest
 		String s2 = "John";
 		System.out.println((s==s2) + " " + s.hashCode() + " " + s2.hashCode());
 		System.out.println((s==s2) + " " + new Object().hashCode() + " " + new Object().hashCode());
-	
+
 	}
 }
