2abd77dffb3bcdf6a12abd96e0641434de963591
javaconsigliere <javaconsigliere@gmail.com>
2019-05-03 10:26:13 -0700

    Changed the meta name to jwt_token

--- src/main/java/org/zoxweb/shared/security/JWTToken.java
+++ src/main/java/org/zoxweb/shared/security/JWTToken.java
@@ -1,7 +1,6 @@
 package org.zoxweb.shared.security;
 
 import org.zoxweb.shared.data.SetNameDescriptionDAO;
-
 import org.zoxweb.shared.util.GetNVConfig;
 import org.zoxweb.shared.util.NVConfig;
 import org.zoxweb.shared.util.NVConfigEntity;
@@ -11,79 +10,68 @@ import org.zoxweb.shared.util.SharedUtil;
 
 @SuppressWarnings("serial")
 public class JWTToken
-extends SetNameDescriptionDAO
-{
-	public enum Param
-	implements GetNVConfig
-	{
-		JWT(NVConfigManager.createNVConfigEntity("jwt", "JWT object", "JWT", true, false, JWT.class, null)),
-		TOKEN(NVConfigManager.createNVConfig("token", "Original token", "Token", true, false, String.class)),
+    extends SetNameDescriptionDAO {
 
-		;
-		
-		private final NVConfig nvc;
-		
-		Param(NVConfig nvc)
-		{
-	        this.nvc = nvc;
-		}
-		
-		public NVConfig getNVConfig() 
-		{
-			return nvc;
-		}
-	}
-	
-	public static final NVConfigEntity NVC_JWT_TOKEN = new NVConfigEntityLocal("jwt_dao", 
-																				null , 
-																				"JWTDAO", 
-																				true, 
-																				false, 
-																				false, 
-																				false, 
-																				JWTToken.class, 
-																				SharedUtil.extractNVConfigs(Param.values()), 
-																				null, 
-																				false, 
-																				SetNameDescriptionDAO.NVC_NAME_DESCRIPTION_DAO);
-	
-	
-	
-	
-	
-	
-	public JWTToken()
-	{
-		super(NVC_JWT_TOKEN);
-	}
+  public enum Param
+      implements GetNVConfig {
+    JWT(NVConfigManager
+        .createNVConfigEntity("jwt", "JWT object", "JWT", true, false, JWT.class, null)),
+    TOKEN(NVConfigManager
+        .createNVConfig("token", "Original token", "Token", true, false, String.class)),
 
-	public JWTToken(JWT jwt, String token)
-	{
-		this();
-		setJWT(jwt);
-		setToken(token);
-	}
-	
-	
-	public JWT getJWT()
-	{
-		return lookupValue(Param.JWT);
-	}
-	
-	public void setJWT(JWT jwt)
-	{
-		setValue(Param.JWT, jwt);
-	}
-	
-	public String getToken()
-	{
-		return lookupValue(Param.TOKEN);
-	}
-	
-	public void setToken(String token)
-	{
-		setValue(Param.TOKEN, token);
-	}
-	
-	
+    ;
+
+    private final NVConfig nvc;
+
+    Param(NVConfig nvc) {
+      this.nvc = nvc;
+    }
+
+    public NVConfig getNVConfig() {
+      return nvc;
+    }
+  }
+
+  public static final NVConfigEntity NVC_JWT_TOKEN = new NVConfigEntityLocal("jwt_token",
+      null,
+      "JWTToken",
+      true,
+      false,
+      false,
+      false,
+      JWTToken.class,
+      SharedUtil.extractNVConfigs(Param.values()),
+      null,
+      false,
+      SetNameDescriptionDAO.NVC_NAME_DESCRIPTION_DAO);
+
+
+  public JWTToken() {
+    super(NVC_JWT_TOKEN);
+  }
+
+  public JWTToken(JWT jwt, String token) {
+    this();
+    setJWT(jwt);
+    setToken(token);
+  }
+
+
+  public JWT getJWT() {
+    return lookupValue(Param.JWT);
+  }
+
+  public void setJWT(JWT jwt) {
+    setValue(Param.JWT, jwt);
+  }
+
+  public String getToken() {
+    return lookupValue(Param.TOKEN);
+  }
+
+  public void setToken(String token) {
+    setValue(Param.TOKEN, token);
+  }
+
+
 }
