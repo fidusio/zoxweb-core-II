c8e8c321e1af28edba29782edc36e2bd25b573fe
javaconsigliere <javaconsigliere@gmail.com>
2019-05-11 23:38:42 -0700

    update

--- src/test/java/org/zoxweb/server/security/SSLSocketPropTest.java
+++ src/test/java/org/zoxweb/server/security/SSLSocketPropTest.java
@@ -17,7 +17,6 @@ package org.zoxweb.server.security;
 
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 
