63b2a89ba233491b8c095ed5ea4dc3eeb2319adb
javaconsigliere <javaconsigliere@gmail.com>
2019-06-16 20:34:18 -0700

    Added Base30 getLong

--- src/main/java/org/zoxweb/shared/util/NumSet.java
+++ src/main/java/org/zoxweb/shared/util/NumSet.java
@@ -1,5 +1,7 @@
 package org.zoxweb.shared.util;
 
+import java.util.UUID;
+
 public abstract class NumSet {
 
   public static final Base30 BASE_30 = new Base30();
@@ -11,6 +13,8 @@ public abstract class NumSet {
 
 
     private Base30(){}
+
+    //public static final String MAX_VALUE = BASE_30.toString(Long.MAX_VALUE);
 
 
     public static final byte[][] SETS = {
@@ -26,25 +30,77 @@ public abstract class NumSet {
         }
     };
 
-    public static final int BASE_VAL = SETS[0].length;
 
     public  String toString(long val)
     {
+      return toString(val, SETS, false, 13);
+    }
 
-      long result = val;
-      long rest;
-      StringBuilder ret = new StringBuilder();
-      do {
-        rest   = result % BASE_VAL;
-        result = result / BASE_VAL;
-        ret.insert(0, (char)SETS[0][(int)rest]);
-      }
-      while(result !=0);
+    public  String toString(UUID uuid)
+    {
+      return toString(uuid.getMostSignificantBits(), SETS, true, 13) +
+             toString(uuid.getLeastSignificantBits(), SETS, true, 13);
+    }
 
-      return ret.toString();
+
+
+    public long getLong(String str)
+    {
+      return getLong(str, SETS);
     }
   }
 
 
+  public static long getLong(String str, byte[][] sets)
+  {
+    int radix = sets[0].length;
+    long ret = 0;
+    byte[] strBytes = SharedStringUtil.getBytes(str);
+    for (int i = 0 ; i < strBytes.length; i++)
+    {
+      byte b = strBytes[strBytes.length-1-i];
+      int val = val(b, sets);
+      ret += val*Math.pow(radix, i);
+    }
+
+    return ret;
+  }
+
+
+  private static int val(byte b, byte[][] sets)
+  {
+    for (int i=0; i < sets[0].length; i++)
+    {
+      if(b == sets[0][i] || b == sets[1][i])
+        return i;
+    }
+    throw new NumberFormatException();
+  }
+
   public abstract String toString(long val);
+
+   protected static String toString(long val, byte[][] sets, boolean addLeadingZero, int maxLength)
+  {
+    int radix = sets[0].length;
+    long result = Math.abs(val);;
+    long rest;
+    StringBuilder ret = new StringBuilder();
+    do {
+      rest   = result % radix;
+      result = result / radix;
+      ret.insert(0, (char)sets[0][(int)rest]);
+    }
+    while(result !=0);
+
+
+    if(addLeadingZero)
+    {
+      while(ret.length() < maxLength)
+      {
+        ret.insert(0, (char)sets[0][0]);
+      }
+    }
+
+    return ret.toString();
+  }
 }
--- src/test/java/org/zoxweb/shared/util/NumSetTest.java
+++ src/test/java/org/zoxweb/shared/util/NumSetTest.java
@@ -1,7 +1,7 @@
 package org.zoxweb.shared.util;
 
+import java.util.UUID;
 import org.junit.Test;
-import org.zoxweb.shared.util.NumSet.Base30;
 
 public class NumSetTest {
 
@@ -9,12 +9,26 @@ public class NumSetTest {
   public void convert()
   {
     long[] longSet = {
-      0, 10, 100, 1000, 10000, 1000000, Long.MAX_VALUE, 1, 2, 4, 8, 16, 31, 32, 64, 128, 256, 512, 1024
+        0, 10, 100, 1000, 10000, 1000000, Long.MAX_VALUE, 1, 2, 4, 8, 16, 31, 32, 64, 128, 256, 512, 1024
     };
 
     for(long val : longSet)
     {
-      System.out.println(val +", " +NumSet.BASE_30.toString(val));
+      String str = NumSet.BASE_30.toString(val);
+      String str1 = "" + NumSet.BASE_30.getLong(str);
+      System.out.println(val +", "  +  str + ", " + str1 + ", " + str1.equals(""+val) + ", converted length: " + str.length());
+
+    }
+  }
+
+
+  @Test
+  public void base30UUID()
+  {
+    for (int i = 0; i < 100; i++) {
+      UUID uuid = UUID.randomUUID();
+      String str = NumSet.BASE_30.toString(uuid);
+      System.out.println(uuid + ", " + str + ", " + str.length());
     }
   }
 }
