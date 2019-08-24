23560213367c7cfd47b02d7b69cbe902ae929fb0
javaconsigliere <javaconsigliere@gmail.com>
2019-08-23 21:51:58 -0700

    Creating HTTPBasicServer

---
+++ src/main/java/org/zoxweb/server/http/HTTPBasicServer.java
@@ -0,0 +1,38 @@
+package org.zoxweb.server.http;
+
+import java.io.IOException;
+import org.zoxweb.shared.http.HTTPServerConfig;
+import org.zoxweb.shared.util.DaemonController;
+
+public class HTTPBasicServer
+  implements DaemonController
+{
+  private HTTPServerConfig config;
+  private boolean isClosed = false;
+
+  public HTTPBasicServer(HTTPServerConfig config)
+  {
+    this.config = config;
+  }
+
+  public void start()
+  {
+
+    isClosed = false;
+  }
+
+
+  @Override
+  public boolean isClosed() {
+    return isClosed;
+  }
+
+  @Override
+  public void close() throws IOException {
+
+    if(!isClosed()) {
+
+      isClosed = true;
+    }
+  }
+}
---
+++ src/main/java/org/zoxweb/shared/http/HTTPServerConfig.java
@@ -0,0 +1,13 @@
+package org.zoxweb.shared.http;
+
+import org.zoxweb.shared.data.PropertyDAO;
+
+
+public class HTTPServerConfig
+extends PropertyDAO
+{
+  public HTTPServerConfig()
+  {
+    super(NVC_PROPERTY_DAO);
+  }
+}
