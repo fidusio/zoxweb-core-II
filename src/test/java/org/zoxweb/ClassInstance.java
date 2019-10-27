be8f6d41d7840d2c7ba07120a244acee557ec880
javaconsigliere <javaconsigliere@gmail.com>
2019-10-26 18:27:51 -0700

    Added test case

---
+++ src/test/java/org/zoxweb/ClassInstance.java
@@ -0,0 +1,22 @@
+package org.zoxweb;
+
+import org.junit.Test;
+import org.zoxweb.shared.data.AddressDAO;
+import org.zoxweb.shared.util.Const;
+import org.zoxweb.shared.util.NVEntity;
+
+public class ClassInstance {
+
+    @Test
+    public void testAssignableFrom()
+    {
+        assert(Enum.class.isAssignableFrom(Const.TimeInMillis.class));
+        assert(NVEntity.class.isAssignableFrom(AddressDAO.class));
+    }
+
+    public void testInstanceOf()
+    {
+        assert(new AddressDAO() instanceof NVEntity);
+    }
+
+}
