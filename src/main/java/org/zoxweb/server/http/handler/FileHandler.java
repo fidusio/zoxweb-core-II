f26dedbb874d1f70b96f928d303cdaae9546d510
javaconsigliere <javaconsigliere@gmail.com>
2020-02-06 18:31:47 -0800

    Create the FileHandler

---
+++ src/main/java/org/zoxweb/server/http/handler/FileHandler.java
@@ -0,0 +1,71 @@
+package org.zoxweb.server.http.handler;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.zoxweb.server.io.IOUtil;
+import org.zoxweb.server.util.GSONUtil;
+import org.zoxweb.shared.data.SimpleDocumentDAO;
+import org.zoxweb.shared.http.HTTPHeaderName;
+import org.zoxweb.shared.http.HTTPMimeType;
+import org.zoxweb.shared.http.HTTPStatusCode;
+import org.zoxweb.shared.util.SharedStringUtil;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.util.logging.Logger;
+
+public class FileHandler implements HttpHandler {
+    private final static Logger log = Logger.getLogger(FileHandler.class.getName());
+
+    public final String baseDir;
+    public FileHandler(String baseDir)
+    {
+        this.baseDir = baseDir;
+    }
+
+    public void handle(HttpExchange he) throws IOException {
+
+
+        String path = he.getHttpContext().getPath();
+
+        URI uri = he.getRequestURI();
+        log.info("user agent:" +  he.getRequestHeaders());
+        log.info("path: " + path);
+        log.info("URI: " +  uri.getPath());
+        log.info("Remote IP: " + he.getRemoteAddress());
+        log.info("Thread: " + Thread.currentThread());
+        try {
+            String filename = uri.getPath().substring(path.length(), uri.getPath().length());
+            log.info("filename: " + filename);
+            HTTPMimeType mime = HTTPMimeType.lookupByExtenstion(filename);
+            log.info("mime type: " + mime);
+
+            if(mime != null)
+                he.getResponseHeaders()
+                        .add(HTTPHeaderName.CONTENT_TYPE.getName(), mime.getValue());
+            File file = new File(baseDir, filename);
+            if (!file.exists() || !file.isFile())
+                throw new FileNotFoundException();
+
+            he.sendResponseHeaders(HTTPStatusCode.OK.CODE, file.length());
+            IOUtil.relayStreams(new FileInputStream(file), he.getResponseBody(), true);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+
+            HTTPStatusCode hsc= HTTPStatusCode.BAD_REQUEST;
+            SimpleDocumentDAO sdd = new SimpleDocumentDAO();
+            sdd.setContent(hsc.CODE + ", " + hsc.REASON + ", not found.");
+            String message = GSONUtil.toJSON(sdd, false, false, false);
+            byte buffer[] = SharedStringUtil.getBytes(message);
+            he.getResponseHeaders()
+                    .add(HTTPHeaderName.CONTENT_TYPE.getName(), HTTPMimeType.APPLICATION_JSON.getValue());
+            he.sendResponseHeaders(hsc.CODE, buffer.length);
+            he.getResponseBody().write(buffer);
+        }
+    }
+}
--- src/test/java/org/zoxweb/server/http/HTTPServerTest.java
+++ src/test/java/org/zoxweb/server/http/HTTPServerTest.java
@@ -5,22 +5,22 @@ import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.net.URI;
+
 import java.util.logging.Logger;
-import org.zoxweb.server.io.IOUtil;
+
+import org.zoxweb.server.http.handler.FileHandler;
 import org.zoxweb.server.logging.LoggerUtil;
 import org.zoxweb.server.task.TaskUtil;
 import org.zoxweb.server.util.GSONUtil;
+
 import org.zoxweb.shared.http.HTTPHeaderName;
 import org.zoxweb.shared.http.HTTPMimeType;
-import org.zoxweb.shared.http.HTTPStatusCode;
+
 import org.zoxweb.shared.util.NVGenericMap;
 import org.zoxweb.shared.util.SharedStringUtil;
 
@@ -54,54 +54,6 @@ public class HTTPServerTest {
   }
 
 
-  static class FileHandler implements HttpHandler {
-
-    private String routDir;
-    public FileHandler(String rootDir)
-    {
-      this.routDir = rootDir;
-    }
-
-    public void handle(HttpExchange he) throws IOException {
-      
-
-      String path = he.getHttpContext().getPath();
-      
-      URI uri = he.getRequestURI();
-      log.info("user agent:" +  he.getRequestHeaders());
-      log.info("path: " + path);
-      log.info("URI: " +  uri.getPath());
-      log.info("Remote IP:" + he.getRemoteAddress());
-      try {
-        String filename = uri.getPath().substring(path.length(), uri.getPath().length());
-        log.info("filename: " + filename);
-        HTTPMimeType mime = HTTPMimeType.lookupByExtenstion(filename);
-        log.info("mime type: " + mime);
-
-        if(mime != null)
-        he.getResponseHeaders()
-            .add(HTTPHeaderName.CONTENT_TYPE.getName(), mime.getValue());
-
-        File file = new File(routDir, filename);
-        if (!file.exists() || !file.isFile())
-          throw new FileNotFoundException();
-
-
-        he.sendResponseHeaders(HTTPStatusCode.OK.CODE, file.length());
-
-        IOUtil.relayStreams(new FileInputStream(file), he.getResponseBody(), true);
-
-      }
-      catch(Exception e)
-      {
-        e.printStackTrace();
-        he.sendResponseHeaders(HTTPStatusCode.BAD_REQUEST.CODE, 0);
-      }
-      
-     
-
-    }
-  }
 
 
   public static void main(String... args) {
@@ -112,7 +64,7 @@ public class HTTPServerTest {
       for (; index < args.length; index++) {
         server.createContext("/" + args[index], new ContextHandler());
       }
-      HttpContext hc = server.createContext("/.well-known/pki-validation/", new FileHandler("/public"));
+//      HttpContext hc = server.createContext("/.well-known/pki-validation/", new FileHandler("/public"));
 //      hc.setAuthenticator(new Authenticator() {
 //        @Override
 //        public Result authenticate(HttpExchange httpExchange) {
@@ -121,6 +73,8 @@ public class HTTPServerTest {
 //      });
       //server.createContext("/toto", new FileHandler());
       server.setExecutor(TaskUtil.getDefaultTaskProcessor());
+
+      HttpContext hc = server.createContext("/", new FileHandler("/temp"));
 
       log.info(hc.getPath());
       server.start();
