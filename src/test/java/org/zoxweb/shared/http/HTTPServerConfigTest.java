609d3643120a369668980a85239a098eba9f4a60
javaconsigliere <javaconsigliere@gmail.com>
2020-04-09 17:37:28 -0700

    update

--- src/test/java/org/zoxweb/shared/http/HTTPServerConfigTest.java
+++ src/test/java/org/zoxweb/shared/http/HTTPServerConfigTest.java
@@ -1,6 +1,8 @@
 package org.zoxweb.shared.http;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import org.junit.jupiter.api.Test;
 import org.zoxweb.server.io.IOUtil;
@@ -11,9 +13,10 @@ public class HTTPServerConfigTest {
   private static final String jsonConfigFile = "http_server_config.json";
   @Test
   public void valueChecks() throws IOException {
-    HTTPServerConfig hsc = GSONUtil.fromJSON(IOUtil.inputStreamToString(HTTPServerConfig.class.getClassLoader().getResourceAsStream(jsonConfigFile), true), HTTPServerConfig.class);
+    HTTPServerConfig hsc = GSONUtil.fromJSON(IOUtil.inputStreamToString(getClass().getClassLoader().getResourceAsStream(jsonConfigFile), true), HTTPServerConfig.class);
     System.out.println(hsc);
     System.out.println(hsc.getConnectionConfigs());
   }
+
 
 }
