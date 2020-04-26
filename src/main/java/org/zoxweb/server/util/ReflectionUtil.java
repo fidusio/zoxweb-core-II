480c4ed787e0486ebd2feba5181f66adb2a3a1bd
javaconsigliere <javaconsigliere@gmail.com>
2020-04-25 23:09:55 -0700

    Added reflection methods

--- src/main/java/org/zoxweb/server/util/ReflectionUtil.java
+++ src/main/java/org/zoxweb/server/util/ReflectionUtil.java
@@ -16,12 +16,14 @@
 package org.zoxweb.server.util;
 
 
+import org.zoxweb.shared.util.MetaToken;
+import org.zoxweb.shared.util.SharedUtil;
+
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 
 /**
@@ -31,6 +33,92 @@ import java.util.Map;
  */
 public class ReflectionUtil
 {
+
+	public static class AnnotationMap
+	{
+
+		private final Class<?> clazz;
+		private Annotation[] classAnnotations;
+		private Map<Method, Annotation[]> methodsAnnotations = new LinkedHashMap<Method, Annotation[]>();
+
+		public AnnotationMap(Class<?> c)
+		{
+			SharedUtil.checkIfNulls("class can't be null", c);
+			this.clazz = c;
+		}
+
+		public Class<?> getAnnotatedClass()
+		{
+			return clazz;
+		}
+
+		public Annotation[] getClassAnnotations()
+		{
+			return classAnnotations;
+		}
+
+		public Map<Method, Annotation[]> getMethodsAnnotations()
+		{
+			return methodsAnnotations;
+		}
+
+		public boolean equals(Object o)
+		{
+			if(o instanceof AnnotationMap)
+				if(((AnnotationMap) o).getAnnotatedClass().equals(getAnnotatedClass()))
+					return true;
+
+
+			return false;
+		}
+
+		public String toString()
+		{
+			return clazz.getName() + "," + Arrays.toString(classAnnotations) + ":" + methodsAnnotations;
+		}
+
+		public boolean isClassAnnotatedBy(Class<? extends Annotation> c)
+		{
+			SharedUtil.checkIfNulls("Class can't be null", c);
+			if(classAnnotations != null) {
+				for (Annotation a : classAnnotations) {
+					if (a.annotationType().equals(c))
+						return true;
+				}
+			}
+			return false;
+		}
+
+		public boolean isMethodAnnotatedBy(Method m, Class<? extends Annotation> c)
+		{
+			SharedUtil.checkIfNulls("Method or class can't be null", m,c);
+			Annotation[] annotations = methodsAnnotations.get(m);
+			if(annotations != null)
+			{
+				for (Annotation a : annotations) {
+					if (a.annotationType().equals(c))
+						return true;
+				}
+			}
+			return false;
+		}
+
+//		public boolean isMethodAnnotatedBy(String m, Class<? extends Annotation> c)
+//		{
+//			SharedUtil.checkIfNulls("Method or class can't be null", m,c);
+//			Annotation[] annotations = methodsAnnotations.get(m);
+//			if(annotations != null)
+//			{
+//				for (Annotation a : annotations) {
+//					if (a.annotationType().equals(c))
+//						return true;
+//				}
+//			}
+//			return false;
+//		}
+
+
+	}
 
 	private ReflectionUtil()
     {
@@ -84,6 +172,47 @@ public class ReflectionUtil
 		}
 		
 		return ret;
+	}
+
+	public static AnnotationMap scanClassAnnotations(Class<?> c, Class<? extends Annotation>...annotationTypes)
+	{
+		AnnotationMap ret = new AnnotationMap(c);
+		ret.classAnnotations =  matchAnnotations(c.getAnnotations(), annotationTypes);
+		Method[] methods = c.getMethods();
+
+
+		for(Method method : methods)
+		{
+			if(Modifier.isPublic(method.getModifiers())) {
+				Annotation[] methodAnnotations = matchAnnotations(method.getDeclaredAnnotations(), annotationTypes);
+				if(methodAnnotations != null)
+					ret.methodsAnnotations.put(method, methodAnnotations);
+			}
+		}
+
+
+		if(ret.classAnnotations != null || ret.methodsAnnotations.size() > 0)
+			return ret;
+
+		return null;
+	}
+
+	public static Annotation[] matchAnnotations(Annotation[] annotations, Class<? extends Annotation>...annotationTypes)
+	{
+		Set<Annotation> match = new HashSet<Annotation>();
+		for(Annotation a : annotations)
+		{
+			for(Class<?> c : annotationTypes){
+				if (a.annotationType().equals(c))
+				{
+					match.add(a);
+					break;
+				}
+			}
+		}
+		if(match.size() > 0)
+			return match.toArray(new Annotation[0]);
+		return null;
 	}
 
 }
---
+++ src/test/java/org/zoxweb/server/util/AnnotationScanTest.java
@@ -0,0 +1,45 @@
+package org.zoxweb.server.util;
+
+import org.junit.jupiter.api.Test;
+import org.zoxweb.shared.annotation.EndPointProp;
+import org.zoxweb.shared.annotation.SecurityProp;
+import org.zoxweb.shared.http.HTTPMethod;
+import org.zoxweb.shared.security.AuthenticationType;
+
+public class AnnotationScanTest {
+
+    @SecurityProp(authentication = {AuthenticationType.DOMAIN}, roles = "admin")
+    public static class ToBeTested
+    {
+        @EndPointProp(uris = "/profile/{profileId}", name = "profileReader", methods = {HTTPMethod.GET})
+        @SecurityProp(authentication = {AuthenticationType.ALL})
+        public String getProfile(String profileId)
+        {
+            return "str";
+        }
+
+        public void noAnnotations(){};
+        @Deprecated
+        private void notToBeReported(){}
+
+
+        @EndPointProp(uris = "/check-user/{user}", name = "CheckUser", methods = {HTTPMethod.POST})
+        @SecurityProp(authentication = {AuthenticationType.ALL}, permissions= "self, admin")
+        public static boolean checkStatus(String user)
+        {
+            return true;
+        }
+
+    }
+
+    @Test
+    public void annotationScan()
+    {
+       ReflectionUtil.AnnotationMap result =  ReflectionUtil.scanClassAnnotations(ToBeTested.class, EndPointProp.class, Deprecated.class, SecurityProp.class);
+       assert(result!=null);
+       assert(result.getClassAnnotations() != null);
+       assert(result.getMethodsAnnotations().size() > 0);
+       assert(result.isClassAnnotatedBy(SecurityProp.class));
+       assert(!result.isClassAnnotatedBy(Deprecated.class));
+    }
+}
